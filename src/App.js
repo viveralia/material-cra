a686afa0d35d2e319cc76705eb4ca6264b1b7571
viveralia <edgarv10@gmail.com>
2020-01-22 17:23:26 -0600

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+
+const App = () => {
+  return (
+    <div>
+      <Typography>Hello</Typography>
+    </div>
+  );
+};
+
+export default App;
